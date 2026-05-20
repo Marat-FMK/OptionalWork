96c1268120588eb663a6c81e68cbb5caa4038631
marat_FMK <kosjachok8@yandex.ru>
2026-05-20 17:50:47 +0300

    //2

--- workTwo.playground/Contents.swift
+++ workTwo.playground/Contents.swift
@@ -1095,7 +1095,14 @@ extension String {
 let testText = "велолев"
 print("Is polindrome - \(testText.isPolindrome())")
 
+//2 - Расширение Int
+//Добавь метод squared() для Int, который возвращает квадрат числа.
 
+extension Int {
+    func squared() -> Int {
+        Int(pow(Double(self), 2))
+    }
+}
 
 //3 - Класс "Person"
 //Создай класс Person с полями name и age.
