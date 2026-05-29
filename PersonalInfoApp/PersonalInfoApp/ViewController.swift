bfcc043b6569730f81d0bcf83ad1405440a53a1e
marat_FMK <kosjachok8@yandex.ru>
2026-05-29 16:39:36 +0300

    //add photo for profile

---
+++ PersonalInfoApp/PersonalInfoApp/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  PersonalInfoApp
+//
+//  Created by Marat Fakhrizhanov on 29.05.2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let photo = UIImageView()
+        photo.frame = CGRect(x: 100, y: 100, width: 200, height: 200)
+        photo.image = .photo
+        
+    }
+
+
+}
+
