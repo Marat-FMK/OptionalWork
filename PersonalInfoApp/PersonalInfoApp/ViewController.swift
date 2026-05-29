714357f2ec8e2684b8e76149878a4a80a1c35a50
marat_FMK <kosjachok8@yandex.ru>
2026-05-29 17:03:16 +0300

    add Views

--- PersonalInfoApp/PersonalInfoApp/ViewController.swift
+++ PersonalInfoApp/PersonalInfoApp/ViewController.swift
@@ -14,13 +14,44 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = .lightGray
+        
+        let nameLabel = UILabel()
+        nameLabel.text = "Mark Markov"
+        nameLabel.textColor = .blue
+        nameLabel.frame = CGRect(x: 250, y: 130, width: 200, height: 20)
+        
+        let ageLabel = UILabel()
+        ageLabel.text = "18"
+        ageLabel.textColor = .blue
+        ageLabel.frame = CGRect(x: 250, y: 170, width: 200, height: 20)
         
         let photo = UIImageView()
-        photo.frame = CGRect(x: 100, y: 100, width: 200, height: 200)
+        photo.frame = CGRect(x: 50, y: 100, width: 150, height: 150)
         photo.image = .photo
         
+        let action = UIAction { _ in
+            print ("Edit button tapped")
+        }
+        
+        let editButton = UIButton(frame: CGRect(x: 60, y: 320, width: 300, height: 30), primaryAction: action)
+        editButton.setTitle("Редактировать профиль", for: .normal)
+        editButton.tintColor = .purple
+        editButton.backgroundColor = . black
+        
+        
+        
+        // Add subviews
+        view.addSubview(photo)
+        view.addSubview(nameLabel)
+        view.addSubview(ageLabel)
+        view.addSubview(editButton)
     }
 
 
+}
+
+#Preview{
+    ViewController()
 }
 
