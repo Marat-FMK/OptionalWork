7650b99b7d3225b4b9d82adaf7093c5715c7f21c
marat_FMK <kosjachok8@yandex.ru>
2026-06-07 22:40:54 +0300

    add settings VC

---
+++ PersonalInfoApp/PersonalInfoApp/SettingsViewController.swift
@@ -0,0 +1,19 @@
+//
+//  SettingsViewController.swift
+//  PersonalInfoApp
+//
+//  Created by Marat Fakhrizhanov on 07.06.2026.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+ 
+
+}
