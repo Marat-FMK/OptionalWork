a0b3c91bc81211895b6df2a31f00238fe0af4272
marat_FMK <kosjachok8@yandex.ru>
2026-06-08 13:41:35 +0300

    set settingsVC

--- PersonalInfoApp/PersonalInfoApp/SettingsViewController.swift
+++ PersonalInfoApp/PersonalInfoApp/SettingsViewController.swift
@@ -9,11 +9,40 @@ import UIKit
 
 class SettingsViewController: UIViewController {
 
+    lazy var nameTF = createTF(placeholder: "Enter your name")
+    lazy var lastName = createTF(placeholder: "Enter your last name")
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        title = "Settings"
+        
+        navigationItem.backBarButtonItem?.title = "General"
         
     }
+    
+    private func createLabel(text: String) -> UILabel {
+        let newLabel = UILabel()
+        newLabel.text = text
+        newLabel.font = .systemFont(ofSize: 16)
+        newLabel.textColor = .gray
+        
+        newLabel.translatesAutoresizingMaskIntoConstraints = false
+        
+        return newLabel
+    }
  
+    private func createTF(placeholder: String ) -> UITextField {
+        let newTF = UITextField()
+        newTF.placeholder = placeholder
+        newTF.font = .systemFont(ofSize: 20)
+        newTF.layer.cornerRadius = 15
+        newTF.textColor = .gray
+        newTF.backgroundColor = .lightGray
+        newTF.translatesAutoresizingMaskIntoConstraints = false
+        
+        return newTF
+    }
 
 }
